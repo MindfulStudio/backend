41482545d814350e9583393bf585d9c274403b35
Baris Balci <barisbalcimusic@gmail.com>
2024-08-22 21:05:03 +0200

    feature/schema: checkinModel updated

--- src/models/checkinModel.js
+++ src/models/checkinModel.js
@@ -4,24 +4,30 @@ const { Schema, model } = mongoose;
 
 const checkinSchema = new Schema(
   {
-    // Reference to User collection
-    userId: {
-      type: mongoose.Types.ObjectId,
-      required: true,
-      ref: "User",
-    },
-    // todayCount: { type: Number }, // NECESSARY?
+    // checkinsToday: { type: Number }, // NECESSARY?
     emotion: {
-      family: String,
-      name: [String],
+      family: {
+        type: String,
+        enum: ["family1", "family2", "family3", "family4"],
+        required: true,
+      },
+      name: { type: String, required: true },
+      isDefault: { type: Boolean, default: true },
+      isActive: { type: Boolean, default: true },
     },
-    tags: {
-      when: [String],
-      where: [String],
-      with: [String],
-      context: [String],
-    },
-    comment: String,
+    tags: [
+      {
+        category: {
+          type: String,
+          enum: ["when", "where", "with", "context"],
+          required: true,
+        },
+        name: [{ type: String, required: true }],
+        isDefault: { type: Boolean, default: true },
+        isActive: { type: Boolean, default: true },
+      },
+    ],
+    comment: { type: String, default: null },
     sleepingHours: Number,
     physicalActivity: Boolean,
     weather: String,

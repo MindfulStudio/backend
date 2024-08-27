6ee0cd060322967523ff243c128aace2280d6899
hannahnier <hannahr@posteo.de>
2024-08-27 11:48:41 +0200

    feature/getUserCustoms: add user controllers for custom items (get all & deactivate one)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,29 +1,25 @@
 import { User } from "../models/userModel.js";
 import { Checkin } from "../models/checkinModel.js";
 
-// GET ALL USERS
-export const getAllUsers = async (req, res, next) => {
-  try {
-    const users = await User.find();
-    if (users?.length === 0) {
-      return res.status(404).json({ message: "No users found" });
-    }
-    res.status(200).json({ data: users });
-  } catch (error) {
-    next(error);
+//////////////////////// ON USER LEVEL ////////////////////////
+
+// BASIC FUNCTIONS
+
+const userNotFound = (res, user, userId) => {
+  if (!user) {
+    res.status(404).json({ message: `User with id [${userId}] not found` });
+    return true; // USER NOT FOUND
   }
+  return false; // USER FOUND
 };
 
 // GET SINGLE USER
+
 export const getSingleUser = async (req, res, next) => {
   try {
-    const { id } = req.params;
-    const user = await User.findById(id);
-    if (!user) {
-      return res
-        .status(404)
-        .json({ message: `User with id [${id}] not found` });
-    }
+    const { userId } = req.params;
+    const user = await User.findById(userId);
+    if (userNotFound(res, user, userId)) return; // ABORT IF USER NOT FOUND
     res.status(200).json({ data: user });
   } catch (error) {
     next(error);
@@ -31,6 +27,7 @@ export const getSingleUser = async (req, res, next) => {
 };
 
 // CREATE NEW USER
+
 export const postUser = async (req, res, next) => {
   try {
     const user = await User.create(req.body);
@@ -44,49 +41,127 @@ export const postUser = async (req, res, next) => {
 };
 
 // UPDATE USER
+
 export const updateUser = async (req, res, next) => {
   try {
-    const { id } = req.params;
+    const { userId } = req.params;
     const update = req.body;
     const options = {
       new: true,
       runValidators: true,
     };
-    const user = await User.findByIdAndUpdate(id, update, options);
-    if (!user) {
-      return res
-        .status(404)
-        .json({ message: `User with id [${id}] not found` });
-    }
+    const user = await User.findByIdAndUpdate(userId, update, options);
+    if (userNotFound(res, user, userId)) return;
     res
       .status(200)
-      .json({ message: `User with id [${id}] updated`, data: user });
+      .json({ message: `User with id [${userId}] updated`, data: user });
   } catch (error) {
     next(error);
   }
 };
 
 // DELETE USER
+
 export const deleteUser = async (req, res, next) => {
   try {
-    const { id } = req.params;
-    const user = await User.findById(id);
-    if (!user) {
-      return res
-        .status(404)
-        .json({ message: `User with id [${id}] not found` });
-    }
+    const { userId } = req.params;
+    const user = await User.findById(userId);
+    if (userNotFound(res, user, userId)) return;
 
     // DELETE (USER'S CHECK-INS)
-    user.checkins.forEach(async (checkin) => {
+    for (let checkin of user.checkins) {
       await Checkin.findByIdAndDelete(checkin._id);
-    });
+    }
 
     // DELETE (USER)
-    const deleted = await User.findByIdAndDelete(id);
+    const deleted = await User.findByIdAndDelete(userId);
     res
       .status(200)
-      .json({ message: `User with id [${id}] deleted`, data: deleted });
+      .json({ message: `User with id [${userId}] deleted`, data: deleted });
+  } catch (error) {
+    next(error);
+  }
+};
+
+//////////////////////// ON CUSTOMS LEVEL ////////////////////////
+
+// GET CUSTOM ITEMS (EMOTIONS & TAGS)
+
+export const getAllCustoms = async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    const user = await User.findById(userId).populate("checkins");
+    if (userNotFound(res, user, userId)) return;
+
+    // CREATE LIST OF USER'S CUSTOM ITEMS:
+    const customs = { emotions: [], tags: [] };
+    user.checkins.forEach((checkin) => {
+      // FOR EACH CHECKIN: IF ITEM IS CUSTOM AND NOT ALREADY IN LIST, ADD TO LIST
+      if (
+        !checkin.emotion.isDefault &&
+        !customs.emotions.some((emo) => emo.name === checkin.emotion.name)
+      ) {
+        customs.emotions.push(checkin.emotion);
+      }
+      // FOR EACH TAG: IF TAG IS CUSTOM AND NOT ALREADY IN LIST, ADD TO LIST
+      checkin.tags.forEach((tag) => {
+        if (!tag.isDefault && !customs.tags.some((t) => t.name === tag.name)) {
+          customs.tags.push(tag);
+        }
+      });
+    });
+    res.status(200).json({ data: customs });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// SOFT-DELETE CUSTOM ITEM (EMOTION OR TAG)
+
+export const deactivateCustom = async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    const { type, name } = req.body;
+    if (!type || !name) {
+      return res.status(400).json({
+        message:
+          "Please provide type and name of the custom item you want to deactivate.",
+      });
+    }
+
+    // FIND USER'S CHECK-INS
+    const user = await User.findById(userId).populate("checkins");
+    if (userNotFound(res, user, userId)) return;
+
+    let isUpdated = false;
+    // MAP THROUGH USER'S CHECK-INS
+    user.checkins.map(async (checkin) => {
+      // IF EMOTION IS CUSTOM AND MATCHES NAME, DEACTIVATE IT
+      if (type === "emotion" && checkin.emotion.name === name) {
+        checkin.emotion.isActive = false;
+        isUpdated = true;
+        return checkin.save();
+      } else if (type === "tag") {
+        // MAP THROUGH TAGS; IF TAG IS CUSTOM AND MATCHES NAME, DEACTIVATE IT
+        checkin.tags.forEach((tag) => {
+          if (tag.name === name) {
+            tag.isActive = false;
+            isUpdated = true;
+            return checkin.save();
+          }
+        });
+      }
+    });
+    // IF ITEM WAS NOT FOUND, RETURN ERROR MESSAGE
+    if (!isUpdated) {
+      return res.status(404).json({
+        message: `Custom item [${name}] of type [${type}] not found.`,
+      });
+    }
+    // RETURN SUCCESS MESSAGE
+    res.status(200).json({
+      message: `Custom item(s) updated successfully.`,
+    });
   } catch (error) {
     next(error);
   }

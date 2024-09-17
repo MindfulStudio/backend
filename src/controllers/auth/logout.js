101f6b6e38a1062dbb127d55736c053edaea4653
hannahnier <hannahr@posteo.de>
2024-09-17 12:06:13 +0200

    fix/login: keep cookie for 1 hour if user does not wish to be remembered, fix logout

--- src/controllers/auth/logout.js
+++ src/controllers/auth/logout.js
@@ -3,7 +3,11 @@
 export const logout = async (req, res, next) => {
   try {
     // CLEAR COOKIE
-    res.clearCookie("accessToken");
+    res.clearCookie("accessToken", {
+      httpOnly: true,
+      sameSite: "Strict",
+      secure: true,
+    });
     res.status(200).json({ message: "Logout successfull" });
   } catch (error) {
     next(error);
